3c7c12483fa59fe7278513ceeeec01ca04b59160
abeldavid <spadabel@hotmail.com>
2015-08-11 14:04:14 -0500

    Moderación de comentarios

--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,8 @@ router.get('/', function(req, res) {
 });
 
 // Autoload de comandos con :quizId
-router.param('quizId', quizController.load); // autoload: quizId
+router.param('quizId', quizController.load);        // autoload: quizId
+router.param('commentId', commentController.load);  // autoload: commentId
 
 // Definición de rutas de sesión
 router.get('/login',  sessionController.new);     // formulario login
@@ -28,8 +29,10 @@ router.get('/quizes/:quizId(\\d+)/edit',    sessionController.loginRequired, qui
 router.put('/quizes/:quizId(\\d+)',         sessionController.loginRequired, quizController.update);
 router.delete('/quizes/:quizId(\\d+)',      sessionController.loginRequired, quizController.destroy);
 
+// Definición rutas de comentatios
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments',    commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish', sessionController.loginRequired, commentController.publish);
 
 // Definición de rutas de /author
 router.get('/author', function(req, res) {
